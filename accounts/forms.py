a7c55cf8487c3e486a60522bc956f01d7a2fe793
CoouDo <67823312+Dong20109@users.noreply.github.com>
2026-05-29 08:38:45 +0900

    Add Form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,13 +1,37 @@
+# accounts/forms.py
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
+
 
 class SignupForm(UserCreationForm):
-    # username = 학번
-    username = forms.CharField(label="학번", max_length=150)
-    first_name = forms.CharField(label="이름", max_length=30)
-    library_code = forms.CharField(label="학생증 도서관 번호", max_length=50)
+    username = forms.CharField(
+        max_length=150,
+        required=True,
+        widget=forms.TextInput(attrs={'placeholder': '학번(5자리)'}),
+        error_messages={
+            'unique': '이미 등록된 학번입니다.'
+        }
+    )
+    first_name = forms.CharField(
+        label="이름",
+        max_length=30,
+        required=True,
+        widget=forms.TextInput(attrs={'placeholder': '이름'}),
+    )
+    email = forms.EmailField(
+        label="이메일 주소",
+        required=True,
+        widget=forms.EmailInput(attrs={'placeholder': '이메일 주소'}),
+        error_messages={
+            'invalid': '올바른 이메일 형식이 아닙니다.'
+        }
+    )
 
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = ("username", "first_name", "library_code", "password1", "password2")
+        fields = ("username", "first_name", "email")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
